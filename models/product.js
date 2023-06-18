8cc39c2854494b254b8824e838bb8058776c2ee7
Sonneeee29112001 <100127182+Sonneeee29112001@users.noreply.github.com>
2023-06-18 17:59:59 +0700

    aaaa

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_CONNECT_URI, {
+mongoose.connect('mongodb+srv://20521841:291101@cluster0.4kmc2yl.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
